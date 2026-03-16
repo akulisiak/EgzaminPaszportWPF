d6d79de6941409ab2227d83fdaebd95daf746c55
Student <Student@TE-KAL-D-S14-03>
2026-03-16 15:14:38 +0100

    Implementacja logiki

--- EgzaminPaszportWPF/MainWindow.xaml.cs
+++ EgzaminPaszportWPF/MainWindow.xaml.cs
@@ -20,5 +20,61 @@ namespace EgzaminPaszportWPF
         {
             InitializeComponent();
         }
+
+        private void UpdatePhoto(object sender, RoutedEventArgs e)
+        {
+            string numer = numerText.Text;
+            
+            if (numer == "000")
+            {
+                zdjecieImg.Source = new BitmapImage(new Uri("img/000-zdjecie.jpg", UriKind.Relative));
+                odciskImg.Source = new BitmapImage(new Uri("img/000-odcisk.jpg", UriKind.Relative));
+            }
+            else if (numer == "111")
+            {
+                zdjecieImg.Source = new BitmapImage(new Uri("img/111-zdjecie.jpg", UriKind.Relative));
+                odciskImg.Source = new BitmapImage(new Uri("img/111-odcisk.jpg", UriKind.Relative));
+            }
+            else if (numer == "333")
+            {
+                zdjecieImg.Source = new BitmapImage(new Uri("img/333-zdjecie.jpg", UriKind.Relative));
+                odciskImg.Source = new BitmapImage(new Uri("img/333-odcisk.jpg", UriKind.Relative));
+            }
+            else
+            {
+                zdjecieImg.Source = null;
+                odciskImg.Source = null;
+            }
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            string numer = numerText.Text;
+            string imie = imieText.Text;    
+            string nazwisko = nazwiskoText.Text;
+            string kolor = "";
+
+            if (niebieskiRadio.IsChecked == true)
+            {
+                kolor = "niebieski";
+            }
+            else if (zielonyRadio.IsChecked == true)
+            {
+                kolor = "zielony";
+            }
+            else if (piwnyRadio.IsChecked == true)
+            {
+                kolor = "piwny";
+            }
+
+            if (numer == "" || imie == "" || nazwisko == "")
+            {
+                MessageBox.Show("Wszystkie pola muszą być wypełnione.");
+            }
+            else
+            {
+                MessageBox.Show($"{imie} {nazwisko}, kolor oczu: {kolor}");
+            }
+        }
     }
 }
